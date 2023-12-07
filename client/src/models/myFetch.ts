77381f14167fa438ba90e8c2fdca5a65894fd4a4
jackgusler <guslerj1@newpaltz.edu>
2023-12-06 21:42:44 -0500

    Add credentials and update CORS headers

--- client/src/models/myFetch.ts
+++ client/src/models/myFetch.ts
@@ -1,11 +1,9 @@
-/* B"H
-*/
-
 const API_ROOT = import.meta.env.VITE_API_ROOT as string;
 
 export function rest(url: string, body?: unknown, method?: string, headers?: HeadersInit){
     return fetch(url, {
         method: method ?? (body ? "POST" : "GET"),
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
             ...headers
@@ -22,12 +20,3 @@ export function rest(url: string, body?: unknown, method?: string, headers?: Hea
 export function api(action: string, body?: unknown, method?: string, headers?: HeadersInit){
     return rest(`${API_ROOT}/${action}`, body, method, headers);
 }
-
-
-
-/*  Asynchronous patterns in JavaScript
-    1. Callbacks
-    2. Pipelining
-    3. Promises
-    4. Async/Await
-*/
--- server/index.js
+++ server/index.js
@@ -20,9 +20,10 @@ app
 
   // CORS
   .use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Origin", "https://cps493final.onrender.com");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
     if (req.method === "OPTIONS") {
       return res.sendStatus(200);
     }
